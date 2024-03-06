68c1c51b0c4eccf83b4354f50ad976d181cfe7b0
falconve <falconve@gmail.com>
2024-03-06 22:39:12 +0700

    Bai 1 & 2

---
+++ bai2.js
@@ -0,0 +1,19 @@
+// Biến dùng để lưu trữ dữ liệu
+// Khai báo biến dùng let và const
+
+// dùng const để khai báo các biến không thay đổi
+const myName = "Tuan anh";
+
+// dùng let để khai báo biến có thay đổi
+let age = 26;
+
+// Cách để khai báo string dùng backticks ``
+let string = `Let's learn js`;
+
+console.log(string);
+// Dùng backtick để làm việc với string và biến dễ dàng hơn
+console.log(`Toi ten la: ${myName}, năm nay tôi ${age}`);
+
+// Dùng thuộc tính length để lấy độ dài của chuỗi (Length là thuộc tính
+// của kiểu dữ liệu string)
+console.log(myName.length);
