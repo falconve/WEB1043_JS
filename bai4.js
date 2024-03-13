0256fcaa5dbb55ca626bbe0bed7cef589c65780f
falconve <falconve@gmail.com>
2024-03-13 19:15:02 +0700

    update bai 4

--- bai4.js
+++ bai4.js
@@ -45,3 +45,41 @@ arrList1.indexOf(2); // kết quả: 1 (1 là vị trí của phần tử). Nế
 
 // [tìm kiếm phần tử cuối cùng trong mảng]
 arrList1.lastIndexOf(2); // kết quả: 3 (3 là vị trí của phần tử). Nếu kết quả là -1 tức là không tìm thấy
+
+// MATH
+
+// Math.max nó sẽ lấy ra giá trị lớn nhất trong dãy truyền vào
+console.log(Math.max(12, 99, 1, 0, 100, 5000));
+
+// Math.min nó sẽ lấy ra giá trị lớn nhất trong dãy truyền vào
+console.log(Math.min(12, 99, 1, 0, 100, 5000));
+
+// Math.sqrt dùng để tìm căn bậc hai của 1 số
+console.log(Math.sqrt(25));
+
+// Math.pow dùng để tìm luỹ thừa của 1 số
+console.log(Math.pow(4, 2));
+
+// Math.round: Nếu sau dấu phẩy là .5 .6 .7 .8 thì sẽ làm tròn lên
+// Nếu sau dấu phẩy là .4 .3 .2 .1 làm tròn xuống
+console.log(Math.round(5.4));
+
+// Math.floor làm tròn xuống
+console.log(Math.floor(5.9)); // Kết quả: 5
+
+// Math.ceil làm tròn lên
+console.log(Math.ceil(5.1)); // Kết quả: 6
+
+// Toán tử NOT ký hiệu !
+console.log(!true); // false
+console.log(!false); // true
+
+// Toán tử &&
+let a = 5;
+let b = 10;
+console.log(a == 5 && b == 10); // true
+
+// Toán tử ||
+let c = 9;
+let d = 20;
+console.log(c == 9 || d < 0); // true
