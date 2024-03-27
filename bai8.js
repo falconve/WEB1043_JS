46ee6c5326d29da74afe98ac4fd8752beb5161e2
falconve <falconve@gmail.com>
2024-03-27 08:29:46 +0700

    bai 8

--- bai8.js
+++ bai8.js
@@ -4,10 +4,10 @@
 // Xác định text node, attribute node
 
 // Query trong DOM
-// Lấy ra thuộc tính id của thẻ html
+// Truy xuất vào element có id là kt_aside
 let eleId = document.getElementById("kt_aside"); // Trả về thẻ HTML có id là kt_aside
 
-// Lấy ra thuộc tính class của thẻ html
+// Truy xuất vào element có class kt_aside
 let eleClass = document.getElementsByClassName("kt_aside");
 
 // Sử dụng querySelector để lấy ra 1 element
@@ -18,5 +18,5 @@ let ElemQueryClass = document.querySelector(".kt_aside"); // Với class thì th
 let elemt = document.querySelector("#reg_pages_msg > a");
 
 // Để lấy ra tất cả thẻ
-// Sử dụng querySelectorAll để lấy ra tất cả element
+// Sử dụng querySelectorAll để lấy ra tất cả thẻ
 let divElement = document.querySelectorAll("div");
